421b89646b7f21c9c64c11d49ee43394fb979e72
Tim Pieniazek <timpieniazek@gmail.com>
2018-03-08 01:14:25 -0500

    Fixed missing main method. Added prompts and scanner inputs.

--- src/deliverables/grandcircus/SameSum.java
+++ src/deliverables/grandcircus/SameSum.java
@@ -3,8 +3,18 @@ package deliverables.grandcircus;
 import java.util.Scanner;
 
 public class SameSum {
-	Scanner scnr = new Scanner(System.in);
-	int input1;
-	int input2;
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+		int input1;
+		int input2;
+		
+		//prompts and inputs from scanner
+		System.out.println("Enter an integer:");
+		input1 = scnr.nextInt();
+		
+		System.out.println("Enter another integer:");
+		input2 = scnr.nextInt();
+		
+	}
 	
 }
