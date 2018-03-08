fcfe8dbd7419e6ddd8347ca8c1ef57795252d332
Tim Pieniazek <timpieniazek@gmail.com>
2018-03-08 01:42:42 -0500

    Added validation for the same number of digits. Added compareDigits method.

--- src/deliverables/grandcircus/SameSum.java
+++ src/deliverables/grandcircus/SameSum.java
@@ -7,14 +7,29 @@ public class SameSum {
 		Scanner scnr = new Scanner(System.in);
 		int input1;
 		int input2;
+		int numberOfDigits;
 		
-		//prompts and inputs from scanner
-		System.out.println("Enter an integer:");
+		
+		//prompts and scanner inputs
+		System.out.println("Please enter an integer:");
 		input1 = scnr.nextInt();
+		numberOfDigits = String.valueOf(input1).length();
 		
-		System.out.println("Enter another integer:");
+		System.out.println("Please enter a second integer:");
 		input2 = scnr.nextInt();
 		
+		//check for same number of digits
+		while (numberOfDigits != String.valueOf(input2).length()) {
+			System.out.println("Your first integer is " + input1 +". Please enter a second integer with the same number of digits:");
+			input2 = scnr.nextInt();
+		}
+		
+		System.out.println(compareDigits(input1, input2));
+	}
+	
+	//method to compare and check 
+	public static boolean compareDigits (int number1, int number2) {
+		return true;
 	}
 	
 }
