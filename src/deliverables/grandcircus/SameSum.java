ca39db47f442b1ef93ae81cff9c12fb95d35506b
Tim Pieniazek <timpieniazek@gmail.com>
2018-03-12 16:01:01 -0400

    Fixed issue where checkSum was casting the char to int, rather than converting.

--- src/deliverables/grandcircus/SameSum.java
+++ src/deliverables/grandcircus/SameSum.java
@@ -1,5 +1,7 @@
 package deliverables.grandcircus;
 
+/* Tim Pieniazek - Deliverable One */
+
 import java.util.Scanner;
 
 public class SameSum {
@@ -9,14 +11,28 @@ public class SameSum {
 		int input2;
 		String digits1;
 		String digits2;
+		int digitSum;
 		
 		/* Prompts user to enter two integers and converts the absolute value to String
 		 * to work with the digits as characters. */
-		System.out.println("Please enter an integer:");
+		
+		// TODO: Add exceptions for not entering an integer
+		System.out.printf("Let's compare two integers with the same number of digits! %n"
+				+ "Each corresponding digit will sum to the same total (True/False): %n");
+		System.out.println("Please enter the first integer:");
+		/* Sends error message if scanner input is not an integer. */
+		while (!scnr.hasNextInt()) {
+			System.out.println("Error. Please enter an integer.");
+			scnr.nextLine();
+		}
 		input1 = scnr.nextInt();
 		digits1 = Integer.toString(Math.abs(input1));
 		
-		System.out.println("Please enter a second integer:");
+		System.out.println("Please enter the second integer:");
+		while (!scnr.hasNextInt()) {
+			System.out.println("Error. Please enter an integer.");
+			scnr.nextLine();
+		}
 		input2 = scnr.nextInt();
 		digits2 = Integer.toString(Math.abs(input2));
 		
@@ -29,24 +45,31 @@ public class SameSum {
 			digits2 = Integer.toString(Math.abs(input2));
 		}
 		
-		System.out.println(compareDigits(digits1, digits2));
+		/* Pull out first digits as char, converts to int and then sums */
+		digitSum = Character.getNumericValue(digits1.charAt(0)) + Character.getNumericValue(digits2.charAt(0));
+		
+		System.out.println(digitSum);
+		
+		/* Output statements to capitalize "True" and "False" */
+		if (compareDigits(digits1, digits2, digitSum) == false) {
+			System.out.printf("False%nEach digit does not sum to the same number.");
+		}
+		else {
+			System.out.printf("True%nEach digit sums to " + digitSum + ".");
+		};
 	}
 	
-	/* Method takes in two integers as strings of the same length */
-	public static boolean compareDigits (String number1, String number2) {
-		int checkSum;
+	/* Method takes in two integers as strings, already checked for the same length */
+	public static boolean compareDigits (String number1, String number2, int checkSum) {
 		int nextSum;
 		int i;
 		boolean sameSum = true;
 		
-		/* Sums the first digit of each integer to compare with the other digits. */
-		checkSum = (int)number1.charAt(0) + (int)number2.charAt(0);
-		
-		/* Starting in the second position, sums the two digits. If not equal, method returns 
-		 * false and breaks loop. Boolean variable is initialized as true, so if the loop completes,
-		 * then each digit must sum to the same number and return true. */
+		/* Starting in the second position, sums the two digits. */
 		for (i = 1; i < number1.length(); ++i) {
-			nextSum = (int)number1.charAt(i) + (int)number2.charAt(i);
+			nextSum = Character.getNumericValue(number1.charAt(i)) + Character.getNumericValue(number2.charAt(i));
+			System.out.println(nextSum);
+			/* If not equal, method returns false and breaks loop. */
 			if (nextSum != checkSum) {
 				sameSum = false;
 				break;
